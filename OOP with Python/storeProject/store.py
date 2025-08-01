f8698b9837d81cb7c750aa6d2fd94a322f3afe48
Ahmet <isleyenahmetcan@gmail.com>
2025-08-01 13:21:18 +0300

    store

---
+++ OOP with Python/storeProject/store.py
@@ -0,0 +1,63 @@
+class Urun:
+    def __init__(self,urunAdi,adet,barkodNo):
+        self.urunAdi = urunAdi
+        self.adet = adet
+        self.barkodNo = barkodNo
+        
+        def __str__(self):
+            f"{self.urunAdi}, {self.adet}, {self.barkodNo}"
+
+def urun_ekle(market):
+    urunAdi = input("Urun Adi: ")
+    adet = input("Adet: ")
+    barkodNo = input("Barkod No: ")
+    yeni_urun = Urun(urunAdi,adet,barkodNo)
+    market.append(yeni_urun)
+    print("Yeni Urun Basariyle Eklendi :)")
+
+def urun_sil(market):
+    if not market:
+        print("Markette Ürün Bulunmuyor")
+        return
+    
+    urun_listele(market)
+    try:
+        secim = input("Silinecek Ürün Numarasi: ")
+        if 0 <=  secim < len(market):
+            silinen = market.pop(secim)
+            print(f"{silinen}, silindi.")
+
+def urun_listele(market):
+    if not market:
+        print("Markette Listelenecek Ürün Yok")
+        return
+    else:
+        for i,urn in enumerate(market):
+            print(f"{i}: {urn}")
+        print()
+
+
+
+def anaMenu():
+    market = []
+
+    while(True):
+        print("## Market Uygulamasina Hoş Geldiniz ##")
+        print("1- Ürün Ekle")
+        print("2- Ürün Sil")
+        print("3- Tüm Ürünleri Listele")
+        print("4- Çikis Yap")
+
+        secim = input("Secim: ")
+
+        if secim == "1":
+            urun_ekle()
+        elif secim == "2":
+            urun_sil()
+        elif secim == "3":
+            urun_listele()
+        elif secim == "4":
+            print("Cikis Yapiliyor")
+            break
+        else:
+            print("Gecerli Bir Deger Giriniz!")
